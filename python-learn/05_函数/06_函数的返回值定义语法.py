cc16a465ef9af25b74eeaf4effc306ba37c8f664
李露 <lilu1@hydsoft.com>
2023-11-09 08:56:58 +0800

    演示：快速体验函数的开发及应用增强

--- python-learn/05_函数/06_函数的返回值定义语法.py
+++ python-learn/05_函数/06_函数的返回值定义语法.py
@@ -12,7 +12,19 @@ def add(a, b):
     print("我完事了")
 
 
+def add_finally(a, b):
+    try:
+        result = a + b
+        # 通过返回值，将相加的结果返回给调用者
+        return result
+    finally:
+        # 返回结果后，还想输出一句话
+        print("我完事了")
+
+
 # 函数的返回值，可以通过变量去接收
 r = add(5, 6)
 print(r)
 
+r = add_finally(5, 6)
+print(f"add_finally:{r}")
--- python-learn/05_函数/07_None.py
+++ python-learn/05_函数/07_None.py
@@ -12,12 +12,12 @@ print(f"无返回值函数，返回的内容类型是：{type(result)}")
 
 # 主动返回None的函数
 def say_hi2():
-    print("你好呀")
+    print("2你好呀")
     return None
 
 result = say_hi2()
-print(f"无返回值函数，返回的内容是：{result}")
-print(f"无返回值函数，返回的内容类型是：{type(result)}")
+print(f"2无返回值函数，返回的内容是：{result}")
+print(f"2无返回值函数，返回的内容类型是：{type(result)}")
 # None用于if判断
 def check_age(age):
     if age > 18:
