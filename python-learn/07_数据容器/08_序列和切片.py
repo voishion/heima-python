f09eb066fcdca87a5d34a764e05fdf7f4a1a94fa
李露 <lilu1@hydsoft.com>
2023-11-10 10:41:42 +0800

    演示：快速体验函数的开发及应用增强

--- python-learn/07_数据容器/08_序列和切片.py
+++ python-learn/07_数据容器/08_序列和切片.py
@@ -17,18 +17,15 @@ my_str = "01234567"
 result3 = my_str[::2]
 print(f"结果3：{result3}")
 
-
 # 对str进行切片，从头开始，到最后结束，步长-1
 my_str = "01234567"
 result4 = my_str[::-1]          # 等同于将序列反转了
 print(f"结果4：{result4}")
 
-
 # 对列表进行切片，从3开始，到1结束，步长-1
 my_list = [0, 1, 2, 3, 4, 5, 6]
 result5 = my_list[3:1:-1]
 print(f"结果5：{result5}")
-
 
 # 对元组进行切片，从头开始，到尾结束，步长-2
 my_tuple = (0, 1, 2, 3, 4, 5, 6)
